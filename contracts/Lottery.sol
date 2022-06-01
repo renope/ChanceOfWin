5f8c03900f188431b80cc80051e661e03bffd3f8
Renope <r.nouripoor@gmail.com>
2022-06-01 21:13:14 +0430

    symplify buyTicket()

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,13 +8,7 @@ import "./Tickets.sol";
 contract Lottery is PriceFeed, ShareFees, Tickets {
 
     function buyTicket(uint16 numberOfTickets) public payable {
-        address ticketBuyer = msg.sender;
-        uint256 paidAmount = msg.value;
-        _checkPaidAmount(paidAmount, numberOfTickets);
-        _register(ticketBuyer);
-        _shareFees(paidAmount * 30 / 100);
-        _collectInPrize(paidAmount * 70 / 100);
-        _purchaseTicket(ticketBuyer, numberOfTickets);
+        buyTicket(numberOfTickets, address(0));
     }
 
     function buyTicket(uint16 numberOfTickets, address referral) public payable {

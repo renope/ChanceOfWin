8e138297640a26adca890b604f614de026e58c97
Renope <r.nouripoor@gmail.com>
2022-06-01 12:29:19 +0430

    numberOfTokens added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PriceFeed.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     
     function ticketPrice() public view returns(uint256) {
         return USDPriceFeed.USD_MATIC_18();
     }
 
     function buyTicket(uint16 numberOfTickets, address referral) public payable {
-        require(msg.value >= ticketPrice() * 95 / 100, "Lottery: insufficient fee");
+        require(msg.value >= numberOfTickets * ticketPrice() * 95 / 100, "Lottery: insufficient fee");
     }
 }

607520fb8e2851adb614bffbc30b46d48ed6475d
Renope <r.nouripoor@gmail.com>
2022-06-01 12:26:16 +0430

    priceFeed completed

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "./PriceFeed.sol";
+
 contract Lottery {
     
-    function ticketFee() public view returns(uint256) {
-        
+    function ticketPrice() public view returns(uint256) {
+        return USDPriceFeed.USD_MATIC_18();
     }
 
     function buyTicket(uint16 numberOfTickets, address referral) public payable {
-        require(msg.value >= tiketPrice(), "Lottery: insufficient fee");
+        require(msg.value >= ticketPrice() * 95 / 100, "Lottery: insufficient fee");
     }
 }
--- contracts/PriceFeed.sol
+++ contracts/PriceFeed.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorInterface.sol";
 
-contract PriceFeed {
+library USDPriceFeed {
 
+    //aggregator on mumbai
+    AggregatorInterface constant AGGREGATOR_MATIC_USD_8 = AggregatorInterface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
+
+    function USD_MATIC_18() internal view returns(uint256) {
+        return uint256(10 ** 26 / AGGREGATOR_MATIC_USD_8.latestAnswer());
+    }
 }

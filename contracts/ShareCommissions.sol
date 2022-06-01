b81fdefce9140550c9b8c344a030ed3919f94f07
Renope <r.nouripoor@gmail.com>
2022-06-01 14:13:46 +0430

    _shareCommissions

--- contracts/ShareCommissions.sol
+++ contracts/ShareCommissions.sol
@@ -5,10 +5,7 @@ abstract contract ShareCommissions {
 
     mapping(address => address) referrals;
 
-    function _deductCommissions(uint256 amount) internal returns(uint256 rest){
-        _pay(owner(), amount * 5 / 100);
-        _pay(team, amount * 5 / 100);
-        _pay(company, amount * 20 / 100);
+    function _shareCommissions(uint256 amount) internal returns(uint256 rest){
     }
 
     function _pay(address receiver, uint256 amount) internal {

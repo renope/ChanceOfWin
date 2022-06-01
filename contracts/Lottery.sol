778ee9aac2500d9045ece01d1ee0e1cb65d8b13f
Renope <r.nouripoor@gmail.com>
2022-06-01 11:15:42 +0430

    PriceFeed added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.7;
 
 contract Lottery {
     
+    function ticketFee() public view returns(uint256) {
+        
+    }
 
     function buyTicket(uint16 numberOfTickets, address referral) public payable {
         require(msg.value >= tiketPrice(), "Lottery: insufficient fee");

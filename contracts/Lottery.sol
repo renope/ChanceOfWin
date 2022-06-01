a4a10154e1b85791092eaa1ab20317f233827dde
Renope <r.nouripoor@gmail.com>
2022-06-01 12:47:22 +0430

    setTicketPriceInCents

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,11 +6,16 @@ import "./PriceFeed.sol";
 
 contract Lottery is Ownable {
     
-    function ticketPrice() public view returns(uint256) {
-        return USDPriceFeed.USD_MATIC_18();
+    uint256 public ticketPriceInCents;
+    function setTicketPriceInCents(uint256 _ticketPriceInCents) public onlyOwner {
+        ticketPriceInCents = _ticketPriceInCents;
+    }
+
+    function ticketPriceInWei() public view returns(uint256) {
+        return USDPriceFeed.USD_MATIC_18() * ticketPriceInCents / 100;
     }
 
     function buyTicket(uint16 numberOfTickets, address referral) public payable {
-        require(msg.value >= numberOfTickets * ticketPrice() * 95 / 100, "Lottery: insufficient fee");
+        require(msg.value >= numberOfTickets * ticketPriceInWei() * 95 / 100, "Lottery: insufficient fee");
     }
 }

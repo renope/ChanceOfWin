5b7bc66ba78c9f93b13a03303c51feb4bf4310c7
Renope <r.nouripoor@gmail.com>
2022-06-01 11:10:15 +0430

    func buyTicket()

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.7;
 
 contract Lottery {
     
+
+    function buyTicket(uint16 numberOfTickets, address referral) public payable {
+        require(msg.value >= tiketPrice(), "Lottery: insufficient fee");
+    }
 }

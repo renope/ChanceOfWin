a7ec4dafdbec143ab25d72482db923a8fdc02655
Renope <r.nouripoor@gmail.com>
2022-06-02 17:24:55 +0430

    deployed to rinkeby

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,23 +2,23 @@ const { ethers, upgrades } = require("hardhat");
 
   async function main() {
     // // Deploying by proxy
-    // const IDCard = await ethers.getContractFactory("IDCard");
-    // const IDC = await upgrades.deployProxy(IDCard);
-    // await IDC.deployed();
-    // console.log("IDCard proxy address:", IDC.address);
+    // const Lottery = await ethers.getContractFactory("Lottery");
+    // const L = await upgrades.deployProxy(Lottery);
+    // await L.deployed();
+    // console.log("Lottery proxy address:", L.address);
   
   
     // // Upgrading
-    // const IDCard = await ethers.getContractFactory("IDCard");
-    // const upgraded = await upgrades.upgradeProxy(IDC.address, IDCard);
+    // const Lottery = await ethers.getContractFactory("Lottery");
+    // const upgraded = await upgrades.upgradeProxy(L.address, Lottery);
     // console.log("upgraded successfully.");
 
 
     // simple deploy
-    const IDCard = await ethers.getContractFactory("IDCard");
-    const IDC = await IDCard.deploy();
-    await IDC.deployed();
-    console.log("IDCard Contract Address:", IDC.address);
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const L = await Lottery.deploy();
+    await L.deployed();
+    console.log("Lottery Contract Address:", L.address);
     
   }
     
